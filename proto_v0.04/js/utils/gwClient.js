c6651ace3d45d5b95d501b5f0c4885036cfdc903
Toni Väisänen <vaisanen.toni@gmail.com>
2017-07-31 10:32:47 +0300

    updated gwClient jdocs

--- proto_v0.04/js/utils/gwClient.js
+++ proto_v0.04/js/utils/gwClient.js
@@ -4,66 +4,77 @@
 
 define(["../configuration/configs"], function (configs) {
     /*
-    * Client for requesting and posting data to graphingwiki
-    * */
+     * Client for requesting and posting data to graphingwiki
+     * */
 
-    // private methods
+    /**
+     * Graphingwiki client module. Contains fetch calls for Graphingwiki actions.
+     * @exports gwClient
+     */
+
+        // private methods
     var moduleName = "GraphingWiki client";
 
+
+    /** @function
+     *  fetchNode
+     *  @param {String} pagename MoinMoin wikipage name
+     *  @return {Promise} Promise of a JSON object containing the node data.
+     */
     function fetchNode(pagename) {
         /*
-     *        Example response
-     *
-     *        GET http://localhost/?action=getGraphJSON&pagename=personA
-     *
-     *        -- response --
-     *        200 OK
-     *        Date:  Wed, 31 May 2017 09:39:36 GMT
-     *        Server:  Apache/2.4.18 (Ubuntu)
-     *        Vary:  Cookie,User-Agent,Accept-Language
-     *        Set-Cookie:  MOIN_SESSION_80_ROOT=9b96593c92f886182f30993f6657a1c5f05c94f4; Expires=Sat, 29-May-2027 09:39:00 GMT; Max-Age=315360000; Path=/
-     *        Content-Length:  780
-     *        Keep-Alive:  timeout=5, max=100
-     *        Connection:  Keep-Alive
-     *        Content-Type:  application/json
-     *
-     *        {
-     *            "debug": "<AllContext ['AllContext']>",
-     *            "data": {
-     *                "in": {
-     *                    "_notype": [
-     *                        "TestUser",
-     *                        "organizationA",
-     *                        "personB"
-     *                    ]
-     *                },
-     *                "acl": "",
-     *                "meta": {
-     *                    "foo": [
-     *                        "[[bar]]"
-     *                    ]
-     *                },
-     *                "mtime": 1496140265.359017,
-     *                "saved": true,
-     *                "out": {
-     *                    "_notype": [
-     *                        "organizationA",
-     *                        "CategoryCategory",
-     *                        "CategoryAsd"
-     *                    ],
-     *                    "gwikicategory": [
-     *                        "CategoryCategory",
-     *                        "CategoryAsd"
-     *                    ],
-     *                    "foo": [
-     *                        "bar"
-     *                    ]
-     *                }
-     *            },
-     *            "response_time": 0.000621795654296875
-     *        }
-     *
-     * */
+         *        Example response
+         *
+         *        GET http://localhost/?action=getGraphJSON&pagename=personA
+         *
+         *        -- response --
+         *        200 OK
+         *        Date:  Wed, 31 May 2017 09:39:36 GMT
+         *        Server:  Apache/2.4.18 (Ubuntu)
+         *        Vary:  Cookie,User-Agent,Accept-Language
+         *        Set-Cookie:  MOIN_SESSION_80_ROOT=9b96593c92f886182f30993f6657a1c5f05c94f4; Expires=Sat, 29-May-2027 09:39:00 GMT; Max-Age=315360000; Path=/
+         *        Content-Length:  780
+         *        Keep-Alive:  timeout=5, max=100
+         *        Connection:  Keep-Alive
+         *        Content-Type:  application/json
+         *
+         *        {
+         *            "debug": "<AllContext ['AllContext']>",
+         *            "data": {
+         *                "in": {
+         *                    "_notype": [
+         *                        "TestUser",
+         *                        "organizationA",
+         *                        "personB"
+         *                    ]
+         *                },
+         *                "acl": "",
+         *                "meta": {
+         *                    "foo": [
+         *                        "[[bar]]"
+         *                    ]
+         *                },
+         *                "mtime": 1496140265.359017,
+         *                "saved": true,
+         *                "out": {
+         *                    "_notype": [
+         *                        "organizationA",
+         *                        "CategoryCategory",
+         *                        "CategoryAsd"
+         *                    ],
+         *                    "gwikicategory": [
+         *                        "CategoryCategory",
+         *                        "CategoryAsd"
+         *                    ],
+         *                    "foo": [
+         *                        "bar"
+         *                    ]
+         *                }
+         *            },
+         *            "response_time": 0.000621795654296875
+         *        }
+         *
+         * */
 
         var requestUrl = configs.API_PATH + pagename;
 
@@ -76,6 +87,11 @@ define(["../configuration/configs"], function (configs) {
         return fetch(nodeRequest);
     }
 
+
+    /** @function
+     *  fetchGraphList
+     *  @return {Promise} Promise of a list containing stored graphs
+     */
     function fetchGraphList() {
         var requestUrl = configs.API_PATH + "graphs";
         // console.log("Loading graphs");
@@ -88,6 +104,11 @@ define(["../configuration/configs"], function (configs) {
         return fetch(loadGraphsRequest);
     }
 
+    /** @function
+     *  fetchPageText
+     *  @param {String} pagename MoinMoin wikipage name
+     *  @return {Promise} Promise of a MoinMoin wikipage content.
+     */
     function fetchPageText(pagename) {
         var requestUrl = configs.API_PATH + pagename + "/text";
         console.log(requestUrl);
@@ -100,6 +121,11 @@ define(["../configuration/configs"], function (configs) {
         return fetch(loadGraphsRequest);
     }
 
+    /** @function
+     *  fetchPageText
+     *  @param {String} graphId of a stored graph
+     *  @return {Promise} Promise of a JSON cytoscape representation of graphId graph.
+     */
     function fetchGraph(graphId) {
         var requestUrl = configs.API_PATH + graphId;
         console.log("GRAPHID: " + graphId);
@@ -115,6 +141,12 @@ define(["../configuration/configs"], function (configs) {
 
     }
 
+    /** @function
+     *  fetchPageText
+     *  @param {String} graphId Id for the graph.
+     *  @param {String} graphToSave cytoscape.json() graph element to save.
+     *  @return {Promise} Promise response of given action.
+     */
     function postGraph(graphId, graphToSave) {
         var developmentPath = configs.API_PATH + 'save/';
         var payload = {
@@ -134,7 +166,13 @@ define(["../configuration/configs"], function (configs) {
         return fetch(saveGraphRequest);
     }
 
-    function postNode(nodeId, content){
+    /** @function
+     *  fetchPageText
+     *  @param {String} nodeId Id for new node. Create new MoinMoin wikipage for this node.
+     *  @param {String} content content for the new page
+     *  @return {Promise} Promise response of given action.
+     */
+    function postNode(nodeId, content) {
         var payload = {
             pagename: nodeId,
             content: "This is page is created by GraphingWikiBrowser" + content
@@ -156,11 +194,11 @@ define(["../configuration/configs"], function (configs) {
     // public methods
     return {
 
-        setConfigs: function(configs){
+        setConfigs: function (configs) {
             setConfiguration(configs);
         },
 
-        getGraph: function(graphId){
+        getGraph: function (graphId) {
             return fetchGraph(graphId);
         },
 
@@ -169,7 +207,7 @@ define(["../configuration/configs"], function (configs) {
             return fetchNode(pagename);
         },
 
-        getGraphList: function(){
+        getGraphList: function () {
             return fetchGraphList();
         },
 
@@ -181,11 +219,11 @@ define(["../configuration/configs"], function (configs) {
             return fetchPageText(pagename);
         },
 
-        postGraph: function(graphId, graphData) {
+        postGraph: function (graphId, graphData) {
             return postGraph(graphId, graphData);
         },
 
-        savePageToMoin: function(nodeId, content){
+        savePageToMoin: function (nodeId, content) {
             return postNode(nodeId, content);
         }
     }
